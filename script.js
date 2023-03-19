489bd96e8a7e064180be6afbd600215200e9a9e3
nreece6 <reece.nick24@gmail.com>
2023-03-19 18:08:46 -0500

    added notes for future work on function

--- script.js
+++ script.js
@@ -3,13 +3,42 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-//create charsets for each perm of possible - prompt for # of chars - ask which chars are needed - feed char # - draw from appropriate charset
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
+//var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+//var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var charsetAll = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var charsetNoSpecial = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+var charsetNoNumber = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var charsetNoSpecialNoNumber = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charsetLowerOnly = "abcdefghijklmnopqrstuvwxyz"
 function writePassword() {
+  var length = window.prompt ("How many characters does your password need to be? Enter a number between 8 and 128.")
+  if (length < 8 || length > 128) {
+    
+  }
+  var useUpper = window.confirm ("Does your password require upper case letters?")
+  var useNumber = window.confirm ("Does your password require numbers?")
+  var useSpecial = window.confirm ("Does your password require special characters?")
+
+  if (useUpper && useNumber && useSpecial) {
+    // generate password with all character
+  } else if (useUpper && useNumber && !useSpecial ) {
+    // generate password with all characters other than special characters
+  } else if (useUpper && !useNumber && useSpecial) {
+    // generate password with all characters other than numbers
+  } else if (!useUpper && useNumber && useSpecial) {
+    // generate password with all characters other than uppercase
+  } else if (useUpper && !useNumber && !useSpecial) {
+    // generate password without number or special characters
+  } else if (!useUpper && useNumber && !useSpecial) {
+    // generate password without uppercase or special characters
+  } else if (!useUpper && !useNumber && useSpecial) {
+    // generate password without uppercase or numbers
+  } else {
+    //generate password with lowercase only
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
