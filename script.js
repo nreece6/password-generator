f0133d6dd8d156c6fec1d150e8c5c661bbd58ee1
nreece6 <reece.nick24@gmail.com>
2023-03-18 19:47:22 -0500

    added potential characters

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
