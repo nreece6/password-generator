2cb4868c38a9cb42d1ba0a3011f80edd191c5fc4
nreece6 <reece.nick24@gmail.com>
2023-03-18 22:43:10 -0500

    added notes on thoughts on solution

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+//create charsets for each perm of possible - prompt for # of chars - ask which chars are needed - feed char # - draw from appropriate charset
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
@@ -11,6 +13,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var passwordChar = window.prompt
   passwordText.value = password;
 
 }
